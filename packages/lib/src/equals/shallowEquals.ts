155bad6d0db507887ab5df2a26aee7e4a6c7b2e8
xxziiko <xxziiko@gmail.com>
2025-07-21 02:46:04 +0900

    refactor: shallowEquals 타입 개선

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,19 +1,17 @@
-import { isObject } from "../utils";
-import { type StringRecord } from "../types";
+import { isObject, isSame } from "../utils";
+import { type ObjectType } from "../types";
 
-export const shallowEquals = (a: StringRecord, b: StringRecord) => {
-  // 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
-  if (a === b) return true;
-
-  // 2. 둘 중 하나라도 객체가 아닌 경우 처리
+export const shallowEquals = (a: unknown, b: unknown): boolean => {
+  if (isSame(a, b)) return true;
   if (!isObject(a) || !isObject(b)) return false;
 
-  // 3. 객체의 키 개수가 다른 경우 처리
-  const aKeys = Object.keys(a);
-  const bKeys = Object.keys(b);
+  const aRecord = a as ObjectType;
+  const bRecord = b as ObjectType;
+  const aKeys = Object.keys(aRecord);
+  const bKeys = Object.keys(bRecord);
 
   if (aKeys.length !== bKeys.length) return false;
 
   // 4. 모든 키에 대해 얕은 비교 수행
-  return aKeys.every((key) => a[key] === b[key]);
+  return aKeys.every((key) => aRecord[key] === bRecord[key]);
 };
