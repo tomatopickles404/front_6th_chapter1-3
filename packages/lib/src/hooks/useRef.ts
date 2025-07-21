e60ba9dd5d27b2685b396ca8c47ac5eff8d863c4
xxziiko <xxziiko@gmail.com>
2025-07-21 15:51:07 +0900

    feat: useMemo 구현

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 type Ref<T> = { current: T };
 
-export function useRef<T>(initialValue: T): Ref<T> {
+export function useRef<T>(initialValue: T): Ref<T | null> {
   const [ref] = useState<Ref<T>>(() => ({
     current: initialValue,
   }));
