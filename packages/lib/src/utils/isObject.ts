191fed8e80c7cf7f4d91105c86cfd83d3dacb31c
xxziiko <xxziiko@gmail.com>
2025-07-22 21:42:10 +0900

    fix: OBJECT_TYPES 상수 제거

--- packages/lib/src/utils/isObject.ts
+++ packages/lib/src/utils/isObject.ts
@@ -1,3 +1,1 @@
-import { OBJECT_TYPES } from "../types";
-
-export const isObject = (value: unknown) => value !== null && typeof value === OBJECT_TYPES.OBJECT;
+export const isObject = (value: unknown) => value !== null && typeof value === "object";
