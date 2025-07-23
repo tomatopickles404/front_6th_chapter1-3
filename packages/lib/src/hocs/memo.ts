6e908fc6d2742ab4c8df7eb3493074bae4272f5d
xxziiko <xxziiko@gmail.com>
2025-07-24 04:01:21 +0900

    feat: memo, deepMemo HOC 구현

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,20 @@
-import { type FunctionComponent } from "react";
+import React, { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks/useRef";
 
+// 핵심은 이전 렌더링 결과를 저장하고 있다가 다음 렌더링 시에 비교하여 변경된 경우에만 다시 렌더링 하는 것
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    const prevPropsRef = useRef<P | null>(null);
+    const prevResultRef = useRef<React.ReactElement | null>(null);
+
+    const propsChanged = !prevPropsRef.current || !equals(prevPropsRef.current, props);
+
+    if (propsChanged) {
+      prevPropsRef.current = props;
+      prevResultRef.current = React.createElement(Component, props);
+    }
+
+    return prevResultRef.current;
+  };
 }
