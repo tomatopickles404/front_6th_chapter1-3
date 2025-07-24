3e47cf899e407141ba2295cfa1ff38ab38a367ec
xxziiko <xxziiko@gmail.com>
2025-07-24 16:10:16 +0900

    feat: useStorage 구현

--- packages/lib/src/hooks/useStorage.ts
+++ packages/lib/src/hooks/useStorage.ts
@@ -1,9 +1,12 @@
 import { useSyncExternalStore } from "react";
 import type { createStorage } from "../createStorage";
+import { useCallback } from "./useCallback";
 
 type Storage<T> = ReturnType<typeof createStorage<T>>;
 
 export const useStorage = <T>(storage: Storage<T>) => {
-  // useSyncExternalStore를 사용해서 storage의 상태를 구독하고 가져오는 훅을 구현해보세요.
-  return storage.get();
+  const { subscribe, get } = storage;
+  const getServerSnapshot = useCallback(() => get(), [get]);
+
+  return useSyncExternalStore(subscribe, get, getServerSnapshot);
 };
