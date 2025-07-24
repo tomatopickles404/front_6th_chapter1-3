e96a6615671cee92c8e88a1d862dee0f678ad0b4
xxziiko <xxziiko@gmail.com>
2025-07-24 19:50:34 +0900

    fix: initialValue 타입 불일치 해결

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 type Ref<T> = { current: T };
 
-export function useRef<T>(initialValue: T): Ref<T | null> {
-  const [ref] = useState<Ref<T>>(() => ({
+export function useRef<T>(initialValue: T | null): Ref<T | null> {
+  const [ref] = useState<Ref<T | null>>(() => ({
     current: initialValue,
   }));
 
